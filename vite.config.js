fa2bcaa2e4fe0a97cfa5b5e8de61be559e3a3d69
Jhon <jgga02@ejemplo.com>
2025-10-29 18:13:45 -0500

    Initial commit: Proyecto ResignificARTE

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        beneficioExpresion: resolve(__dirname, 'beneficio-expresion.html'),
+        beneficioSanacion: resolve(__dirname, 'beneficio-sanacion.html'),
+        beneficioPsicologico: resolve(__dirname, 'benficio-psicologico.html'),
+      }
+    }
+  }
+})
